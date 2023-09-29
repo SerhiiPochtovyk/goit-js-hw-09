01f20875775a8cdf31fd92ac6f8136e0fa8ed073
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-09-29 15:37:04 +0300

    fix task-02;

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,23 +1,26 @@
-import Notiflix from 'notiflix';
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-let clickDate;
+import 'flatpickr/dist/flatpickr.min.css';
+
+let clickedDate;
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
     if (selectedDates[0].getTime() < options.defaultDate.getTime()) {
-      Notiflix.Notify.failure('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
 
       refs.startBtn.disabled = true;
     } else {
       refs.startBtn.disabled = false;
     }
-    clickDate = selectedDates[0].getTime();
+
+    clickedDateedDate = selectedDates[0].getTime();
   },
 };
 
@@ -34,13 +37,15 @@ const refs = {
 refs.startBtn.addEventListener('click', handleTimer);
 
 function handleTimer() {
-  const pickedDate = clickDate;
   const startTimer = setInterval(() => {
     const deltaTime = pickedDate - Date.now();
+
     if (deltaTime < 1000) {
       clearInterval(startTimer);
     }
+
     const timeLeft = convertMs(deltaTime);
+
     updateTimer(timeLeft);
   }, 1000);
 }
@@ -53,17 +58,24 @@ function updateTimer({ days, hours, minutes, seconds }) {
 }
 
 function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
+  return String(value).padStart(2, 0);
 }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
+
+  // Remaining days
   const days = Math.floor(ms / day);
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
   return { days, hours, minutes, seconds };
 }
